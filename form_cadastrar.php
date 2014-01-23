4f8b885eca629a0afb3fd8cca1111684a696ff43
misterhub <luan_100_0@hotmail.com>
2014-01-23 18:01:26 -0200

    Create form_cadastrar.php

---
+++ form_cadastrar.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Cadastro de Pessoas - Cadastrar</title>
+    </head>
+    <body>
+        <?php require_once('menu.php'); ?>
+        
+        <form action="cadastrar.php" method="post">
+            <?php require_once('campos_pessoa.php'); ?>
+        </form>
+    </body>
+</html>
