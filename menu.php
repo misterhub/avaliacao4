ab73a2372c7af264a03bb2b34d88d89a3300482b
misterhub <luan_100_0@hotmail.com>
2014-01-23 18:03:24 -0200

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,6 @@
+<center>
+<a href="index.php">Listar |</a>
+<a href="form_cadastrar.php">Cadastrar |</a>
+<a href="form_editar.php">Editar |</a>
+<a href="form_remover.php">Remover |</a>
+<br/><br/>
