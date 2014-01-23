8cb16c116563b887f0f53fc654b7bd9b77b3f164
misterhub <luan_100_0@hotmail.com>
2014-01-23 17:57:15 -0200

    Create campos_pessoa.php

---
+++ campos_pessoa.php
@@ -0,0 +1,26 @@
+<center>
+<label><b>Nome:</b> </label><input name="nome" type="text" />
+<br/>
+<label><b>CPF:</b> </label><input name="cpf" type="text" />
+<br/>
+<label><b>Nascimento:</b> </label><input name="nascimento" type="text" />
+<br/>
+<label><b>Sexo:</b> </label>&nbsp;
+<input name="sexo" type="radio" value="Masculino" /><label>Masculino</label>
+<input name="sexo" type="radio" value="Feminino" /><label>Feminino</label>
+<br/>
+<label><b>Estado:</b> </label>
+<select name="estado">
+    <option value="-1">Selecione...</option>
+    <option>Bahia</option>
+    <option>São Paulo</option>
+    <option>Rio de Janeiro</option>
+</select>
+<br/>
+<label><b>Telefone:</b> </label><input name="telefone" type="text" />
+<br/>
+<label><b>Observações:</b> </label><br/>
+<textarea name="observacoes" rows="7" cols="40"></textarea>
+<br/><br/>
+<input type="submit" value="Enviar" />
+</center>
